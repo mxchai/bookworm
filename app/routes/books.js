453fd168651283604fd26b2671ed20e2e7625305
MX Chai <mingxuan@live.com.sg>
2015-07-29 10:36:27 +0800

    old way committed as reference

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return Ember.RSVP.hash({
+      books: [
+        {
+          title: 'Awesome book'
+        },
+        {
+          title: 'Not so good one'
+        }
+      ]
+    });
+  },
+
 });
