6c97dcc08493452df834e60e997ec8afa85ad8c3
MX Chai <mingxuan@live.com.sg>
2015-07-29 10:41:04 +0800

    followed tutorial code now

--- app/routes/books.js
+++ app/routes/books.js
@@ -14,4 +14,13 @@ export default Ember.Route.extend({
     });
   },
 
+  setupController: function(controller, model) {
+    controller.setProperties(model);
+    // Why I think this sets books into the controller is that 'model' in this case
+    // is just a reference to the model property above in the model hook i.e. it's returning a hash
+    // So it's something like controller.setProperties(books: [{}, {}])
+    // Therefore, the reference in controller is called books
+    // Also if you do setup controller, the default behaviour which is
+    // controller.set('model', model) doesn't execute
+  }
 });
