447e0ef01582d25dd6cb2b2b552d99cbc782cd40
MX Chai <mingxuan@live.com.sg>
2015-07-29 16:02:03 +0800

    http mock nice

---
+++ server/mocks/books.js
@@ -0,0 +1,54 @@
+module.exports = function(app) {
+  var express = require('express');
+  var booksRouter = express.Router();
+
+  var books = [
+    {
+      id: 1,
+      title: 'Hamlet',
+      description: 'Some stupid book written long ago'
+    },
+    {
+      id: 2,
+      title: 'The quick brown fox',
+      description: 'Jumps over the lazy dog is the best book ever'
+    },
+    {
+      id: 3,
+      title: '1984',
+      description: 'Scary world to live in, the 1984 world is.'
+    }
+  ];
+
+  booksRouter.get('/', function(req, res) {
+    res.send({
+      'books': books
+    });
+  });
+
+  booksRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  booksRouter.get('/:id', function(req, res) {
+    res.send({
+      'books': {
+        id: req.params.id
+      }
+    });
+  });
+
+  booksRouter.put('/:id', function(req, res) {
+    res.send({
+      'books': {
+        id: req.params.id
+      }
+    });
+  });
+
+  booksRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/books', booksRouter);
+};
